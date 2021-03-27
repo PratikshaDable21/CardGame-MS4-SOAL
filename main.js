f6b24b7ee3172cb4441cc8f04eb3c8ac6363b557
Tushar Dighe <dighetushar654@gmail.com>
2021-03-27 19:53:40 +0530

    updated files

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+        "use strict"
         var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
         var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
         var deck = new Array();
